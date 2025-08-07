e903ae02cab17305c7b36a8c9ef3d9a48f22316c
Denis Cardoso <denisilva09@gmail.com>
2025-08-07 08:17:14 -0300

    formatação

--- terraform/modules/ec2_instance/main.tf
+++ terraform/modules/ec2_instance/main.tf
@@ -14,14 +14,14 @@ data "aws_ami" "amazon_linux" {
 }
 
 resource "aws_instance" "web_server" {
-  ami           = data.aws_ami.amazon_linux.id
-  
+  ami = data.aws_ami.amazon_linux.id
+
   instance_type = var.instance_type
-  
+
   vpc_security_group_ids = [var.security_group_id]
-  
+
   key_name = var.key_name
-  
+
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
